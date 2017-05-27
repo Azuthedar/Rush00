197f7882425256165c510585f8aa78309bbc6fe1
Luke HAMLYN <lhamlyn@e4r23p3.jnb.42.fr>
2017-05-27 11:19:09 +0200

    Added GameObject

---
+++ classes/AGameObject.cpp
@@ -0,0 +1,77 @@
+#include "AGameObject.hpp"
+
+AGameObject::AGameObject()
+{
+
+}
+
+AGameObject::AGameObject(AGameObject const & src)
+{
+    *this = src;
+}
+
+AGameObject::~AGameObject()
+{
+
+}
+
+std::string AGameObject::getType()const
+{
+    return this->_type;
+}
+
+void AGameObject::setType(std::string type)
+{
+    this->_type = type;
+}
+
+int AGameObject::getXPos()const
+{
+    return this->_xPos;
+}
+
+void AGameObject::setXPos(int x)
+{
+    this->_xPos = x;
+}
+
+int AGameObject::getYPos()const
+{
+    return this->_yPos;
+}
+
+void AGameObject::setYPos(int y)
+{
+    this->_yPos = y;
+}
+
+int AGameObject::getLives()const
+{
+    return this->_lives;
+}
+
+void AGameObject::setLives(int life)
+{
+    this->_lives =life;
+}
+
+std::string AGameObject::getShape()const
+{
+    return this->_shape;
+}
+
+void AGameObject::setShape(std::string name)
+{
+    this->_shape = name;
+}
+
+AGameObject& AGameObject::operator=(AGameObject const & src)
+{
+    this->_shape = src.getShape();
+    this->_lives = src.getLives();
+    this->_yPos = src.getYpos();
+    this->_xPos = src.getXPos();
+    this->_type = src.getType();
+
+    return (*this)
+}
